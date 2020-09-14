574d442fcf87c2df52018549291b159e336e0642
ReginaF2012 <skritschy@gmail.com>
2020-09-14 17:59:33 -0500

    fixed typo in jwt encode secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
       # the token to be given to a user upon successful signup/login
       def encode_token(payload)
         #! For deployment on Heroku use ENV['SECRET_KEY_BASE']
-        JWT.encode(payload, Rails.application.credentials.secret_key_base)
+        JWT.encode(payload, ENV['SECRET_KEY_BASE'])
       end
 
       # Since token is sent back in a header this is how we access the token
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::API
           # The Begin/Rescue syntax allows us to rescue out of an exception in Ruby.
           begin
             # This will decode the payload we originally gave the token
-            JWT.decode(token, Rails.application.credentials.secret_key_base, true, algorithm: 'HS256')
+            JWT.decode(token, ENV['SECRET_KEY_BASE'], true, algorithm: 'HS256')
           rescue JWT::DecodeError
             nil
           end
